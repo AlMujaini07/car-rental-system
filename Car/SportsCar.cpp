d2b87a2563fffd5867506b55d0cbfee9c0715438
Hamed AlMujaini <hamad.almujaini@hotmail.com>
2026-05-11 23:21:13 +0400

    Sports Car Class added

---
+++ Car/SportsCar.cpp
@@ -0,0 +1,49 @@
+#include "SportsCar.h"
+
+// Default Constructor
+SportsCar::SportsCar(){}
+
+// Parameterized Constructor
+SportsCar::SportsCar(string Brand, string Model, string SubModel, int Year, double PricePerDay,
+    string NumberPlate, bool AvailabilityStatus, double TotalKmDriven,
+    double KmDrivenThisRental, double FuelLevel, int NumSeats, string Color,
+    int horsepower, int Torque, bool hasTurbo, int TopSpeed, double ZeroToHundred)
+    : Car(Brand, Model, SubModel, Year, PricePerDay, NumberPlate, AvailabilityStatus,
+          TotalKmDriven, KmDrivenThisRental, FuelLevel, NumSeats, Color),
+      horsepower(horsepower), Torque(Torque), hasTurbo(hasTurbo), TopSpeed(TopSpeed), ZeroToHundred(ZeroToHundred) {}
+
+// Destructor
+SportsCar::~SportsCar() {}
+
+// Getters
+int SportsCar::getHorsepower() { return horsepower; }
+int SportsCar::getTorque() { return Torque; }
+bool SportsCar::gethasTurbo() { return hasTurbo; }
+int SportsCar::getTopSpeed() { return TopSpeed; }
+double SportsCar::getZeroToHundred() { return ZeroToHundred; }
+
+// Setters
+void SportsCar::setHorsepower(int Horsepower) { this->horsepower = Horsepower; }
+void SportsCar::setTorque(int Torque) { this->Torque = Torque; }
+void SportsCar::sethasTurbo(bool hasTurbo) { this->hasTurbo = hasTurbo; }
+void SportsCar::setTopSpeed(int TopSpeed) { this->TopSpeed = TopSpeed; }
+void SportsCar::setZeroToHundred(double ZeroToHundred) { this->ZeroToHundred = ZeroToHundred; }
+
+// getPrice - overridden
+double SportsCar::getPrice() {
+    return getPricePerDay();
+}
+
+// displayInfo - overridden
+void SportsCar::displayInfo() {
+    Car::displayInfo();
+    cout << "Horsepower: " << horsepower << " HP" << endl;
+    cout << "Torque: " << Torque << " Nm" << endl;
+    cout << "Has Turbo: ";
+    if (hasTurbo)
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+    cout << "Top Speed: " << TopSpeed << " km/h" << endl;
+    cout << "0-100 km/h: " << ZeroToHundred << " seconds" << endl;
+}
---
+++ Car/SportsCar.h
@@ -0,0 +1,42 @@
+#ifndef SporstCar_h
+#define SportsCar_h
+#include "Car.h"
+
+class SportsCar : public Car { 
+    private:
+    int horsepower;
+    int Torque;
+    bool hasTurbo;
+    int TopSpeed;
+    double ZeroToHundred;
+    
+    public:
+    SportsCar(); // Default Constructor
+    SportsCar(string Brand, string Model, string SubModel,int Year, double PricePerDay,
+        string NumberPlate, bool AvailabilityStatus, double TotalKmDriven,
+        double KmDrivenThisRental, double FuelLevel, int NumSeats, string Color, int horsepower,
+        int Torque, bool hasTurbo, int TopSpeed, double ZeroToHundred);
+    ~SportsCar(){} // Destrcutor
+
+    // setters
+    void setHorsepower(int Horsepower);
+    void setTorque(int Torque);
+    void sethasTurbo(bool hasTurbo);
+    void setTopSpeed(int TopSpeed);
+    void setZeroToHundred(double ZeroToHundred);
+
+    //getters
+    int getHorsepower();
+    int getTorque();
+    bool gethasTurbo();
+    int getTopSpeed();
+    double getZeroToHundred();
+
+    //methods - overriden
+    double getPrice() override;
+    void displayInfo() override;
+    
+};
+
+
+#endif
